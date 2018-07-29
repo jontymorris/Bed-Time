ad2c6d56afa2c971a98ede5fcfc06e36ffe26eee
Jonty Morris <jonty.morris@yahoo.co.nz>
2018-07-29 20:05:49 +1200

    Fixed typo

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="BedtimeProject",
-    decription="Shuts down Windows when it is bedtime",
+    description="Shuts down Windows when it is bedtime",
     author="Jonty Morris",
     url="github.com/jontymorris/bed-time",
     packages=find_packages()
