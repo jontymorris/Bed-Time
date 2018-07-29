06cf9d69be96e9e751ea6928e686aec1678c830c
Jonty Morris <jonty.morris@yahoo.co.nz>
2018-07-29 19:23:12 +1200

    Added unit testing

---
+++ bedtime-test.py
@@ -0,0 +1,21 @@
+import unittest, datetime, bedtime
+
+class TestIsBedtime(unittest.TestCase):
+
+    def test_sunday(self):
+        time = datetime.datetime(2018, 1, 7, hour=21, minute=20)
+        self.assertFalse( bedtime.is_bedtime(time), "Sunday 9:20pm is not bed time" )
+        
+        time = datetime.datetime(2018, 1, 7, hour=21, minute=30)
+        self.assertTrue( bedtime.is_bedtime(time), "Sunday 9:30pm is bed time" )
+    
+    def test_friday(self):
+        time = datetime.datetime(2018, 1, 5, hour=22)
+        self.assertFalse( bedtime.is_bedtime(time), "Friday 10pm is not bed time" )
+
+        time = datetime.datetime(2018, 1, 5, hour=23)
+        self.assertTrue( bedtime.is_bedtime(time), "Friday 11pm is bed time" )
+
+
+if __name__ == "__main__":
+    unittest.main()
