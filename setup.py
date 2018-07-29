feddb964857f455bc2fee6b44ab1fae829c64492
Jonty Morris <jonty.morris@yahoo.co.nz>
2018-07-29 19:39:02 +1200

    Fixed Travis-CI

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="BedtimeProject",
+    decription="Shuts down Windows when it is bedtime",
+    author="Jonty Morris",
+    url="github.com/jontymorris/bed-time",
+    packages=find_packages()
+)
